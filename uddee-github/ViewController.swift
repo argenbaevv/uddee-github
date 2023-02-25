a158991e513970c1841f8b40a35f61d12065d6f1
argenbaevv <argenbaev@icloud.com>
2023-02-25 06:41:32 +0300

    Initial Commit

---
+++ uddee-github/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uddee-github
+//
+//  Created by argenbaev on 2/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
